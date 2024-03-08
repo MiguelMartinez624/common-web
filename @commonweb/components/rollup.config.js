1c7192a2fe751d8f85d8c31b1e46a2f7cfc75d4c
miguel <miguel@miguels-MacBook-Pro.local>
2024-03-08 18:23:54 -0400

    feat:template responsive

--- @commonweb/components/rollup.config.js
+++ @commonweb/components/rollup.config.js
@@ -20,7 +20,7 @@ export default [
             terser({
                 mangle: true,
                 toplevel: true,
-                compress: {arrows: true}
+                compress: {arrows: true,drop_console:true}
             })
         ],
         output: [
